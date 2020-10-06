51adf200b58a416424dcaa20833c87363749521a
Bartek <bart.matusz@gmail.com>
2020-10-06 02:22:30 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation ("log4j:log4j:1.2.17")
+    compile ("org.jetbrains.kotlin:kotlin-reflect:1.3.61")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
